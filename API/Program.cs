c6445b2ff3027dc22951a27097e45d018a26f80e
Inna Borodina <creator.geek@gmail.com>
2023-01-22 13:20:19 +0300

    Adding CRUD handlers with CQRS pattern

--- API/Program.cs
+++ API/Program.cs
@@ -1,5 +1,9 @@
+using Application.Activities;
+using Application.Core;
+using MediatR;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
+using System.Linq.Expressions;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,20 +13,22 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
 builder.Services.AddDbContext<DataContext>(opt => 
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
-
-// builder.Services.AddCors(opt => 
-// {
-//     opt.AddPolicy("CorsPolicy", policy => {
-//         policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
-//     });
-// });
+builder.Services.AddCors(opt =>
+{
+    opt.AddPolicy("CorsPolicy", policy =>
+    {
+        policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
+    });
+});
+builder.Services.AddMediatR(typeof(List.Handler));
+builder.Services.AddAutoMapper(typeof(MappingProfiles).Assembly);
 
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -31,7 +37,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-//app.UseCors("CorsPolicy");
+app.UseCors("CorsPolicy");
 
 app.UseAuthorization();
 
--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,39 +1,35 @@
-import React, { useEffect, useLayoutEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import react from 'react';
 import axios from 'axios';
 import { Header } from 'semantic-ui-react';
-import List from 'semantic-ui-react/dist/commonjs/elements/List';
-import ListItem from 'semantic-ui-react/dist/commonjs/elements/List/ListItem';
 
-function App() {
-  const [activities, setActivities] = useState([]);
+export function App() {
+    const [activities, setActivities] = useState([]);
 
-  useEffect(() => {
-    axios.get('http://localhost:5000/api/activities')
-      .then(response => {
-        console.log(response);
-        setActivities(response.data);
-      })
-  }, [])
+    useEffect(() => {
+        axios.get('http://localhost:5000/api/activities')
+            .then(response => {
+                console.log(response);
+                setActivities(response.data);
+            })
+    }, [])
 
   return (
     <div>
       <Header as='h2' icon='users' content='Reactivities' />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello!
-        </p>
-        <List>
-          {activities.map((activity: any) => (
-            <List.Item key={activity.id}>
-              {activity.title}
-            </List.Item>
-          ))}
-        </List>
+          Hello, React!
+          </p>
+          <ul>
+              {activities.map((activity: any) => (
+                  <li key={activity.id}>
+                      {activity.title}
+                  </li>
+                  ))}
+          </ul>
     </div>
   );
 }
-
 export default App;
